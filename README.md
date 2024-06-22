7d7b4a7dba71e83565a9b9de567f2e314ab9ee89
Negar Ebrahimi <114069193+Negarebr@users.noreply.github.com>
2024-06-22 03:33:20 +0330

    Add project.

---
+++ README.md
@@ -0,0 +1,45 @@
+# User Cards Display
+
+This project is a simple web application that fetches and displays user information from an API. Users can click the "Load More" button to fetch more user cards dynamically.
+
+## Features
+
+- Fetches user data from the Dummy JSON API.
+- Displays user information in flip card format.
+- Dynamically loads more users when the "Load More" button is clicked.
+
+## Technologies Used
+
+- HTML
+- CSS
+- JavaScript (ES6)
+- Fetch API
+
+## Setup
+
+Follow these steps to set up the project locally:
+
+1. **Clone the repository**
+
+```sh
+git clone https://github.com/Negarebr/UsersCard.git
+
+cd UsersCard
+```
+
+2. **Open the HTML file**
+
+Open `index.html` in a web browser to view the application.
+
+## Usage
+
+- The application will automatically fetch and display 30 users when the page loads.
+- Click the "Load More" button to fetch and display more users.
+
+## Acknowledgements
+
+Designed by Negar Ebrahimi.
+
+## License
+
+This project is licensed under the MIT License. See the LICENSE file for details.
---
+++ main.js
@@ -0,0 +1,74 @@
+
+
+
+document.addEventListener('DOMContentLoaded', ()=>{
+    const userContainerDiv = document.getElementById('user-container');
+    async function fetchUsers() {
+        const res = await fetch('https://dummyjson.com/users');
+        const json = await res.json();
+        Users(json.users);
+    }
+
+    const more = document.getElementById('more');
+    more.addEventListener('click', ()=>{
+        fetchUsers();
+    });
+
+    function Users(users) {
+        users.forEach((user) => {
+            const flipCardDiv = document.createElement('div');
+            flipCardDiv.classList.add('flip-card');
+            userContainerDiv.appendChild(flipCardDiv);
+
+            const flipCardInnerDiv = document.createElement('div');
+            flipCardInnerDiv.classList.add('flip-card-inner');
+            flipCardDiv.appendChild(flipCardInnerDiv);
+
+            const flipCardFrontDiv = document.createElement('div');
+            flipCardFrontDiv.classList.add('flip-card-front');
+            flipCardInnerDiv.appendChild(flipCardFrontDiv);
+
+            const genderIconDiv = document.createElement('div');
+            genderIconDiv.classList.add('gender-icon');
+            flipCardFrontDiv.appendChild(genderIconDiv);
+
+            const genderIconImg = document.createElement('img');
+            genderIconImg.src = user.gender === 'male' ? 'img/man.png' : 'img/woman.png';
+            genderIconDiv.appendChild(genderIconImg);
+
+            const profileImageDiv = document.createElement('div'); 
+            profileImageDiv.classList.add('profile-image');
+            flipCardFrontDiv.appendChild(profileImageDiv);
+
+            const profileImageImg = document.createElement('img');
+            profileImageImg.classList.add('pfp');
+            profileImageImg.src = user.image;
+            profileImageDiv.appendChild(profileImageImg);
+
+            const nameDiv = document.createElement('div');
+            nameDiv.classList.add('name');
+            nameDiv.textContent = user.firstName + ' ' + user.maidenName + ' ' + user.lastName;
+            profileImageDiv.appendChild(nameDiv);
+
+            const flipCardBackDiv = document.createElement('div');
+            flipCardBackDiv.classList.add('flip-card-back');
+            flipCardInnerDiv.appendChild(flipCardBackDiv);
+
+            const descriptionDiv = document.createElement('div');
+            descriptionDiv.classList.add('Description');
+            flipCardBackDiv.appendChild(descriptionDiv);
+
+            const descriptionP = document.createElement('p');
+            descriptionP.classList.add('description-p');
+            descriptionP.innerHTML = `<strong>Email:</strong> <a href="mailto:${user.email}">${user.email}</a><br>
+                                        <strong>Phone:</strong> <a href="tel:${user.phone}">${user.phone}</a><br>
+                                        <strong>Country:</strong> ${user.address.country} <br>
+                                        <strong>Address:</strong> ${user.address.street}, ${user.address.city}, ${user.address.state} <br>
+                                        <strong>Age:</strong> ${user.age} years old <br>
+                                        <strong>Birthdate:</strong> ${user.birthDate}`;
+            descriptionDiv.appendChild(descriptionP);
+        });
+    }
+
+    fetchUsers();
+});
